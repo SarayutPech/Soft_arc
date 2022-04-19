1560f086e2ccc30d52ce1102a6eff6e1b22374fb
SarayutPech <88422955+SarayutPech@users.noreply.github.com>
2022-04-19 18:18:40 +0700

    Add files via upload

---
+++ Behavior2/HeightSortStrategy.java
@@ -0,0 +1,13 @@
+package Behavior2;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class HeightSortStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparing(Person::getHeight));
+    }
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+
+}
